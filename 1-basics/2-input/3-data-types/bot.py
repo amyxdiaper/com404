f1daeee7f934b15a1b9defd3c7a20634b04f9218
amyxdiaper <43602534+amyxdiaper@users.noreply.github.com>
2018-09-26 12:36:32 +0100

    Update bot.py
    extended code to include more examples of data types

--- 1-basics/2-input/3-data-types/bot.py
+++ 1-basics/2-input/3-data-types/bot.py
@@ -2,3 +2,15 @@
 print("How old are you?")
 age = int(input())
 print("you are" ,age, "years old")
+
+print("What is your height in m?")
+height = float(input())
+print("Your height is", height, "m")
+
+print("What is your weight in kg?")
+weight = float(input())
+print("Your weight is", weight, "kg")
+
+BMI = weight / (height*height)
+
+print("your BMI is",BMI,"")
