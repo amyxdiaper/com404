f700abd40aa688d53b8a255a3dda8c3ab381e2cb
amyxdiaper <43602534+amyxdiaper@users.noreply.github.com>
2018-09-26 10:43:15 +0100

    Update bot.py
    added code to display a long string

--- 1-basics/1-output/2-multiline-message/bot.py
+++ 1-basics/1-output/2-multiline-message/bot.py
@@ -1,3 +1,11 @@
-#display message to the standard output
+#display message to the standard output on multiple lines
 print("System Failure Imminent!")
 print("Attemping reboot.")
+
+#display message to the standart output on multiple lines
+print("""
+Running Sytem tests...
+...Testing motors [Passed]
+...Testing sensors [Passed]
+...Tesing hover engine [Passed]
+""")
