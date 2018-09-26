71baa876e8d95dfcc05a876f8fe8d873d237864d
amyxdiaper <43602534+amyxdiaper@users.noreply.github.com>
2018-09-26 12:03:38 +0100

    Create 2-ascii-robot/bot.py
    added code to show how an input can change an ascii art image

---
+++ 1-basics/1-input/2-ascii-robot/bot.py
@@ -0,0 +1,24 @@
+# Display a robot
+print("      ---------")
+print("    #|  o   o  |#") 
+print("""      ---------
+      /|     |\\
+     / |     | \\
+       \\    /              ((+))
+        ----                 |
+-----------------------------|--------
+""")
+
+print("Please enter a different character for the eyes")
+eye = input()
+
+# Display a robot
+print("      ---------")
+print("    #| ",eye," ",eye," |#") 
+print("""      ---------
+      /|     |\\
+     / |     | \\
+       \\    /              ((+))
+        ----                 |
+-----------------------------|--------
+""")
