99f71d3c522c1bede8b289c21ecc3c2f7efe658c
amyxdiaper <43602534+amyxdiaper@users.noreply.github.com>
2018-10-03 09:59:26 +0100

    Create 3-decision/1-if/bot.py
    added code to demonstrate the use of an if statement

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,12 @@
+# ask user to choose a direction
+print("please enter a direction:")
+print("w to move up,")
+print("a to move left,")
+print("s to move down,")
+print("d to move right.")
+
+direction = input()
+
+# work out which direction to travel
+if (direction == "w"):
+ print("I am moving forward")
