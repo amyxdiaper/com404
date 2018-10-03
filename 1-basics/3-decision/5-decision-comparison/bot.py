9cc4f2ff76845f1cb7bea4cea61c9ede83ce6b42
amyxdiaper <43602534+amyxdiaper@users.noreply.github.com>
2018-10-03 10:57:43 +0100

    Create 5-decision-comparison/bot.py
    added code to demonstrate decision comparison, reading multiple user inputs

---
+++ 1-basics/3-decision/5-decision-comparison/bot.py
@@ -0,0 +1,11 @@
+print("Enter your first number")
+first_number = int(input())
+print("Enter your second number")
+second_number = int(input())
+
+if (first_number > second_number):
+  print("The first number is bigger!")
+elif (first_number < second_number):
+  print("The second number is bigger!")
+else:
+  print("They are equal!")
