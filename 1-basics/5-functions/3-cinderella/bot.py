d99fba920e210299f7251c3128472931a5010229
amyxdiaper <43602534+amyxdiaper@users.noreply.github.com>
2018-10-17 10:14:46 +0100

    Create 3-cinderella/bot.py
    added code to demonstrate the use of parameters within a new function

---
+++ 1-basics/5-functions/3-cinderella/bot.py
@@ -0,0 +1,14 @@
+def try_glass_slipper_on(person):
+ if (person == "Cinderella"):
+  print("I found my princess!")
+ elif (person == "Eldest step sister"):
+  print("You are not my princess. Your feet are too big!")
+ elif (person == "Youngest step sister"):
+  print("You are not my princess. Your feet are too small!")
+ else:
+  print("I don't know who you are but you are not my princess!")
+  
+# The following are calls to the function for testing purposes
+try_glass_slipper_on("Eldest step sister")
+try_glass_slipper_on("Youngest step sister")
+try_glass_slipper_on("Cinderella")
