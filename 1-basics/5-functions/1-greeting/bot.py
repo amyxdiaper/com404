e8ff6a94aa24ecd9a3888eab55d7e19d56f42818
amyxdiaper <43602534+amyxdiaper@users.noreply.github.com>
2018-10-17 09:50:57 +0100

    Create 5-functions/1-greeting/bot.py
    added code to demonstrate how to create a new function

---
+++ 1-basics/5-functions/1-greeting/bot.py
@@ -0,0 +1,9 @@
+# create a new function to greet the user
+def greet_user():
+  print("Please enter your name")
+  name = str(input())
+  print("Hello", name)
+
+greet_user()
+greet_user()
+greet_user()
