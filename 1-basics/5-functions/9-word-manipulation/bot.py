964aed837ddd8b158b6b813f4c0c2669a2467cb3
Amy Diaper <43602534+amyxdiaper@users.noreply.github.com>
2018-10-24 09:09:13 +0100

    Update bot.py
    changed code so that all of it works

--- 1-basics/5-functions/9-word-manipulation/bot.py
+++ 1-basics/5-functions/9-word-manipulation/bot.py
@@ -19,34 +19,34 @@ def mirrored(word):
 def repeat(word):
   print("Please enter number of repitions")
   user_num = int(input())
-  for count in range (0, user_num, 1)
-  if (count / 2 == 0):
-    print(word.lower())
-  else:
-    print(word.upper())
+  for count in range (0, user_num, 1):
+   if (count % 2 == 0):
+     print(word.lower())
+   else:
+     print(word.upper())
 
 
 def run():
-  print("Please enter a word"):
- word = str(input())
- print("""Please choose an option
- 1- put word in an ascii box
- 2- show word in lower case letters
- 3- show word in upper case letters
- 4- show word mirrored
- 5- repeat the word as many times as you like, alternating between upper and lower case""")
- user_option = int(input())
- if (user_option == "1"):
-   print(in_a_box(word))
- elif (user_option == "2"):
-   print(lower_case(word))
- elif (user_option == "3"):
-   print(upper_case(word))
- elif (user_option == "4"):
-   print(mirrored(word))
- elif (user option == "5"):
-   print(repeat(word))
+  print("Please enter a word")
+  word = str(input())
+  print("""Please choose an option
+  1- put word in an ascii box
+  2- show word in lower case letters
+  3- show word in upper case letters
+  4- show word mirrored
+  5- repeat the word as many times as you like, alternating between upper and lower case""")
+  user_option = int(input())
+  if (user_option == 1):
+    print(in_a_box(word))
+  elif (user_option == 2):
+    print(lower_case(word))
+  elif (user_option == 3):
+    print(upper_case(word))
+  elif (user_option == 4):
+    print(mirrored(word))
+  elif (user_option == 5):
+    print(repeat(word))
   else:
     print ("I am not sure what you entered!")
 
-run
+run()
