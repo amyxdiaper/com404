3ae66e905d9c819f4d83b82b0a6bdb0d8f7b2579
Amy Diaper <43602534+amyxdiaper@users.noreply.github.com>
2018-10-31 11:02:13 +0000

    Update main.py
    added code to import child classes to main

--- 2-guis/1-classes-and-objects/main.py
+++ 2-guis/1-classes-and-objects/main.py
@@ -1,10 +1,18 @@
 from bot import Bot
+from superbot import SuperBot
+from flyingbot import FlyingBot
 
 beep = Bot("Beep")
 beep.display_name()
 beep.display_age()
 beep.display_energy()
 beep.display_shield()
-
-
 print(beep)
+
+astro = SuperBot("Astro")
+astro.display_super_power_level()
+print(astro)
+
+boop = FlyingBot("Boop")
+boop.display_hover()
+print(boop)
